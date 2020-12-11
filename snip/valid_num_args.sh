df678210841926cf29954025327596d6fd8e37a1
satu-n <49983831+satu-n@users.noreply.github.com>
2020-12-11 13:54:46 +0900

    snip: refined valid_num_args.sh

--- snip/valid_num_args.sh
+++ snip/valid_num_args.sh
@@ -2,13 +2,11 @@
 
 function valid_num_args() {
   if [ $# -ne 2 ]; then
-    {
-      echo 'usage: valid_num_args $# {expected}'
-    } 1>&2
+    echo 'usage: valid_num_args $# {expected}' 1>&2
     return 1
   fi
   if [ $1 -ne $2 ]; then
-      {
+    {
       echo "$1 arguments found."
       echo "Specify exactly $2 arguments."
     } 1>&2
