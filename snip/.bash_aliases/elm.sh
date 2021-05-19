d4772703a3e800d6f5e9aeae370896ca155ad8c6
satu-n <49983831+satu-n@users.noreply.github.com>
2021-05-19 16:43:12 +0900

    snip: fixed css scripts

--- snip/.bash_aliases/elm.sh
+++ snip/.bash_aliases/elm.sh
@@ -2,4 +2,4 @@
 
 alias elml='elm_make_link_css'
 alias elmjs='elm make src/Main.elm --output=elm.js'
-alias scss='sass css/style.scss css/style.css'
+alias scss='sass src/scss/style.scss style.css'
--- snip/.bashrc/elm_make_link_css.sh
+++ snip/.bashrc/elm_make_link_css.sh
@@ -5,7 +5,7 @@
 function elm_make_link_css() {
   valid_num_args $# 0
   local _src_file="src/Main.elm"
-  local _css_file="css/style.css"
+  local _css_file="style.css"
   local _css_pattern="<style>body { padding: 0; margin: 0; }</style>"
   local _css_replacement="<link rel=\"stylesheet\" type=\"text/css\" href=\"$_css_file\">"
   elm make $_src_file
